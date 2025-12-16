137e64e557eb0e2deb80f8d295aad120e706bc64
BiancaElenaPiu <bianca-elena.piu@student.tuiasi.ro>
2025-12-16 11:25:58 +0200

    bias function

--- EvolutionaryAlgorithm.py
+++ EvolutionaryAlgorithm.py
@@ -52,6 +52,26 @@ def adjustment_algorithm(alpha_genes, y_train, C):
     return alpha.tolist()
 
 
+def compute_bias(alpha, X, y, C, kernel_func):
+    SV_indices = np.where((alpha > 0) & (alpha <= C))[0]
+
+    if len(SV_indices) == 0:
+        return 0.0, 0
+    
+    #b=1/l sum(y_i-sum(y_i*alpha_j*<xi,xj>))
+    K=kernel_func(X,X)
+    alpha_y=alpha*y
+
+    sum= np.dot(K, alpha_y)
+    y_SV= y[SV_indices]
+    sum_SV=sum[SV_indices]
+    b=y_SV- sum
+    b=np.mean(b)
+
+    return b, len(SV_indices)
+
+
+
 class IOptimizationProblem:
     def compute_fitness(self, chromosome):
         raise NotImplementedError("This method needs to be implemented by a subclass")
